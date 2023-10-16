a114ca07b69e4a009c44f269e43499aa99d10e2b
sohag16030 <38885449+sohag16030@users.noreply.github.com>
2023-10-16 08:39:01 +0600

    sliding window problem solve

--- Basic_DSA/src/main/java/WellInterview/SlidingWindow/BestTimeToBuyAndSellStock.java
+++ Basic_DSA/src/main/java/WellInterview/SlidingWindow/BestTimeToBuyAndSellStock.java
@@ -2,6 +2,17 @@ package WellInterview.SlidingWindow;
 
 public class BestTimeToBuyAndSellStock {
     public static void main(String[] args) {
+        int[] price = {2, 7, 3, 5, 13, 1, 6, 8, 4};
 
+        //minBuyPriceSofar = 1
+        //maxProfitSofar = 0
+
+        int minBuyPriceSofar = price[0]; //7
+        int maxProfitsoFar = 0; //0
+
+        for (int i = 0; i < price.length; i++) {
+            minBuyPriceSofar = Math.min(minBuyPriceSofar, price[i]);
+            maxProfitsoFar = Math.max(maxProfitsoFar, price[i] - minBuyPriceSofar);
+        }
     }
 }
