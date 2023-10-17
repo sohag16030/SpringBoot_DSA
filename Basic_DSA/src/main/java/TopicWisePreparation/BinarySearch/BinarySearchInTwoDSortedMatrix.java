af804371388382e38064e8193888ae1fc815cba4
sohag16030 <38885449+sohag16030@users.noreply.github.com>
2023-10-17 23:22:05 +0600

    2d array sorted data binary search done

---
+++ Basic_DSA/src/main/java/TopicWisePreparation/BinarySearch/BinarySearchInTwoDSortedMatrix.java
@@ -0,0 +1,22 @@
+package TopicWisePreparation.BinarySearch;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class BinarySearchInTwoDSortedMatrix {
+    public static void main(String[] args) {
+        int[][] matrix = {{1, 3, 5, 7, 8}, {10, 11, 16, 20, 21}, {23, 30, 34, 60, 61}};
+        int target = 3;
+        int n = 5;
+        int m = 4;
+        int l = 0, h = (n * m) - 1;
+        while (l <= h) {
+            int mid = l + (h - l) / 2;
+            if (matrix[mid / m][mid % m] == target)
+                System.out.println(mid);
+            else if (target < matrix[mid / m][mid % m])
+                h = h - 1;
+            else l = l + 1;
+        }
+    }
+}
--- Basic_DSA/src/main/java/TopicWisePreparation/BinarySearch/SearchInRotatedArrayList.java
+++ Basic_DSA/src/main/java/TopicWisePreparation/BinarySearch/SearchInRotatedArrayList.java
@@ -2,7 +2,29 @@ package TopicWisePreparation.BinarySearch;
 
 public class SearchInRotatedArrayList {
     public static void main(String[] args) {
-        int[] nums = {4, 5, 6, 0, 1, 2, 3};
+        int[] nums = {40, 45, 50, 60, 10, 15, 20, 30, 35};
+        int l = 0, h = nums.length - 1, target = 10;
 
+        while (l <= h) {
+            int mid = l + (h - l) / 2; // 4
+            if (nums[mid] == target) System.out.println("Yes");
+
+            //  build logic for right side first..then reverse for left side..
+            //  check right side data sorted or not
+
+            if (nums[h] >= nums[mid]) {
+                if (target > nums[mid] && target <= nums[h]) {
+                    l = mid + 1;
+                } else {
+                    h = mid - 1;
+                }
+            } else {
+                if (target >= nums[l] && target < nums[mid]) {
+                    h = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            }
+        }
     }
 }
