77ca00a5a719bf8df5c0b124a7ee12d9f795c669
sohag16030 <38885449+sohag16030@users.noreply.github.com>
2023-10-17 08:41:21 +0600

    // 2,3,-1,-2,4,5,-3,5,6,1
            // all positive just multiply
            // jodi negative number are even count....then just multiply
            // jodi odd count hoi thole j kono ek side theke ekta negative num er agr pojonto ans. eta forward hote pare , abr back direction thekeo hote pare..
            // jodi 0 theke thole 0 hole 1 dia shuru korte hbe.. number of 0 , number o partition..
            // 2,3,-1,-10,0,1,-1,5,6,1
    
            // 2,3,4,5,6,1,1
            // 2,1,-3,5,6,-1,6
            // 2,1,-3,5,-4,6,-1,6
    
            // 2,1,-3,5,0,-4,6,-1,6

--- Basic_DSA/src/main/java/WellInterview/SlidingWindow/MaximumProductSubArray.java
+++ Basic_DSA/src/main/java/WellInterview/SlidingWindow/MaximumProductSubArray.java
@@ -1,4 +1,23 @@
 package WellInterview.SlidingWindow;
 
 public class MaximumProductSubArray {
+    public static void main(String[] args) {
+        // 2,3,-1,-2,4,5,-3,5,6,1
+
+        // all positive just multiply
+        // jodi negative number are even count....then just multiply
+        // jodi odd count hoi thole j kono ek side theke ekta negative num er agr pojonto ans. eta forward hote pare , abr back direction thekeo hote pare..
+        // jodi 0 theke thole 0 hole 1 dia shuru korte hbe.. number of 0 , number o partition..
+        // 2,3,-1,-10,0,1,-1,5,6,1
+
+        // 2,3,4,5,6,1,1
+        // 2,1,-3,5,6,-1,6
+        // 2,1,-3,5,-4,6,-1,6
+
+        // 2,1,-3,5,0,-4,6,-1,6
+
+
+
+
+    }
 }
