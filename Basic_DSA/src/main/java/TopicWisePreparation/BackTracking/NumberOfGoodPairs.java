f8a0c5a14a864fdcf895ae77349683399459d800
Md Sohag <sohag.ali@valmetpartners.com>
2023-10-19 14:24:54 +0600

    DFS returning understanding and backtracking understanding.

---
+++ Basic_DSA/src/main/java/TopicWisePreparation/BackTracking/NumberOfGoodPairs.java
@@ -0,0 +1,15 @@
+package TopicWisePreparation.BackTracking;
+
+public class NumberOfGoodPairs {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 1, 1, 3};
+        System.out.println(numIdenticalPairs(nums));
+    }
+
+    public static int numIdenticalPairs(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+
+        }
+        return 0;
+    }
+}
--- Basic_DSA/src/main/java/TopicWisePreparation/BackTracking/Test.java
+++
@@ -1,4 +0,0 @@
-package TopicWisePreparation.BackTracking;
-
-public class Test {
-}
