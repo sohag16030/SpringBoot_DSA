12105da0f2143453cd054b3cda776913044fe2a8
sohag16030 <38885449+sohag16030@users.noreply.github.com>
2023-10-19 23:23:01 +0600

    not solved yet. {0} is present in a list..

--- Basic_DSA/src/main/java/TopicWisePreparation/BackTracking/Subsets.java
+++ Basic_DSA/src/main/java/TopicWisePreparation/BackTracking/Subsets.java
@@ -1,7 +1,30 @@
 package TopicWisePreparation.BackTracking;
 
+import java.util.*;
+
 public class Subsets {
     public static void main(String[] args) {
-        
+        int[] nums = {0};
+        List<List<Integer>> resultSetList = new ArrayList<>();
+        Set<ArrayList<Integer>> seen = new HashSet<>();
+        subsetBacktrack(nums, new ArrayList<Integer>(), resultSetList, 0, seen);
+        System.out.println(resultSetList);
+    }
+
+    private static void subsetBacktrack(int[] input, ArrayList<Integer> curr, List<List<Integer>> resultSetList, int start, Set<ArrayList<Integer>> seen) {
+        if (unique(curr, seen))
+            resultSetList.add(new ArrayList<>(curr));
+        for (int i = start; i < input.length; i++) {
+            curr.add(input[i]);
+            System.out.println(curr);
+            subsetBacktrack(input, curr, resultSetList, i + 1, seen);
+            curr.remove(curr.size() - 1);
+        }
+    }
+
+    private static boolean unique(ArrayList<Integer> curr, Set<ArrayList<Integer>> seen) {
+        Collections.sort(curr);
+        if (!seen.contains(curr)) return true;
+        return false;
     }
 }
