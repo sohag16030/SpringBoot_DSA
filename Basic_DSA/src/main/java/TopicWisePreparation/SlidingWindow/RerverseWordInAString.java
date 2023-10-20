90ff25362fb76e9866825efe2fb6ff048c01beaf
Md Sohag <sohag.ali@valmetpartners.com>
2023-10-20 14:40:27 +0600

    changed

---
+++ Basic_DSA/src/main/java/TopicWisePreparation/SlidingWindow/RerverseWordInAString.java
@@ -0,0 +1,22 @@
+package TopicWisePreparation.SlidingWindow;
+
+public class RerverseWordInAString {
+    public static void main(String[] args) {
+        String s = "Let's take LeetCode contest";
+        System.out.println(reverseWords(s));
+    }
+
+    public static String reverseWords(String s) {
+        String[] strArr = s.split(" ");
+        String result = "";
+        for (int i = 0; i < strArr.length; i++) {
+            System.out.println(strArr[i]);
+            StringBuilder str = new StringBuilder(strArr[i]).reverse();
+            if (i < strArr.length - 1)
+                result += str + " ";
+            else result += str;
+        }
+
+        return result;
+    }
+}
