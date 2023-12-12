9b3d744cdb8c9de3371a0591ee1163dd88e2c6f7
Md Sohag <sohag.ali@valmetpartners.com>
2023-12-12 11:55:46 +0600

    @CollectionId not working..

--- WarmUp/HibernateWarmUP/Java Brains/Hibernate/src/main/java/com/example/hibernateproject/hibernateTest/dto/UserDetails.java
+++ WarmUp/HibernateWarmUP/Java Brains/Hibernate/src/main/java/com/example/hibernateproject/hibernateTest/dto/UserDetails.java
@@ -7,10 +7,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.*;
 import org.hibernate.annotations.CollectionId;
-
 import java.util.ArrayList;
 import java.util.Collection;
-
 import org.hibernate.annotations.Type;
 
 @Data
@@ -23,9 +21,11 @@ public class UserDetails {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int userId;
     private String userName;
-    @ElementCollection(fetch = FetchType.LAZY)
+    @ElementCollection(fetch=FetchType.LAZY)
     @JoinTable(name = "USER_ADDRESS",
-            joinColumns = @JoinColumn(name = "USER_ID"))
+    joinColumns = @JoinColumn(name = "USER_ID"))
+//    @GenericGenerator(name = "hilo-gen", strategy="hilo")
+//    @CollectionId(columns = @Column(name = "ADDRESS_ID"),type = @Type(type ="long"))
     private Collection<Address> listOfAddress = new ArrayList<>();
 
     public UserDetails(String userName) {
